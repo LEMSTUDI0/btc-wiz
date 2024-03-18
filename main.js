324f405f49c65e6b14a93db25b12604cf12ad02f
mateusz <a@a.com>
2024-03-18 23:45:13 +0100

    dodany gradient radialny

--- main.js
+++ main.js
@@ -18,7 +18,6 @@ var light;
 var lightD1;
 var lightD2;
 
-// Funkcja aktualizująca światło na podstawie senpcondaryColor
 function updateLightColor() {
     if (light && lightD1) {
         light.color.set(secondaryColor); // Aktualizacja światła półkulistego
@@ -31,6 +30,52 @@ function updatematerialColor() {
 
 }
 
+// Definicja materiału shaderowego dla tła z gradientem
+var backgroundShaderMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+        colorTop: { type: "c", value: new THREE.Color(0x000000) },
+        colorBottom: { type: "c", value: new THREE.Color(0x000000) },
+        aspectRatio: { type: "f", value: window.innerWidth / window.innerHeight }
+    },
+    vertexShader: `
+        varying vec2 vUv;
+        void main() {
+            vUv = uv;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+    `,
+    fragmentShader: `
+    uniform vec3 colorTop;
+uniform vec3 colorBottom;
+uniform float aspectRatio;
+varying vec2 vUv;
+
+void main() {
+    vec2 center = vec2(0.5, 0.5); // Środek płaszczyzny UV
+    vec2 uvOffset = vUv - center;
+    // Skalowanie UV do zachowania kształtu koła na różnych proporcjach ekranu
+    uvOffset.x *= aspectRatio;
+    // Obliczanie odległości od środka (z normalizacją dla koła)
+    float distance = length(uvOffset) * 2.0; // *2.0, aby rozciągnąć gradient na całą płaszczyznę
+    // Używanie smoothstep do stworzenia miękkiego gradientu z dominacją colorBottom na środku
+    float mixFactor = smoothstep(0.05, 1.0, distance); // Rozpoczęcie od 10% od środka
+
+    vec3 color = mix(colorBottom, colorTop, mixFactor); // Odwrócenie kolejności mieszania
+    gl_FragColor = vec4(color, 1.0);
+}
+`,
+
+    side: THREE.DoubleSide
+});
+
+
+// Funkcja aktualizująca kolory tła
+function updateBackgroundColors() {
+        backgroundShaderMaterial.uniforms.colorTop.value.set(colormain);
+        backgroundShaderMaterial.uniforms.colorBottom.value.set(secondaryColor);
+}
+
+
 async function fetchBitcoinRainbowColor() {
     const response = await fetch('http://localhost:3000/data');
     const data = await response.json();
@@ -38,12 +83,14 @@ async function fetchBitcoinRainbowColor() {
     currentRainbowStatusIndex = data.rainbowStatus;
     console.log("Color: ", data.rainbowStatus);
     // Set background color of the renderer
-    renderer.setClearColor(colormain);
+  // renderer.setClearColor(colormain);
     // Dodatkowa logika do aktualizacji secondaryColor
     // i wywołania funkcji updateLightColor
     updateLightColor(); // Możesz wywołać tę funkcję tutaj, jeśli chcesz od razu zaktualizować kolor
     updatematerialColor();
     console.log('glowny kolor', colormain);
+    console.log('drugi kolor', secondaryColor);
+
 }
 
 async function fetchBitcoinData() {
@@ -77,6 +124,7 @@ async function fetchBitcoinData() {
     } catch (error) {
         console.error('Error fetching data:', error);
     }
+    updateBackgroundColors()
 }
 
 
@@ -86,6 +134,8 @@ fetchBitcoinData();
 setInterval(fetchBitcoinRainbowColor, 60 * 1000);
 
 setInterval(fetchBitcoinData, 10 * 1000);
+
+
 
 // Select the canvas element with the id 'scene'
 var canvas = document.querySelector('#scene');
@@ -99,11 +149,21 @@ var renderer = new THREE.WebGLRenderer({
 // Create a new scene
 var scene = new THREE.Scene();
 
-
 var camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 10000);
+
 
 // Set camera position
 camera.position.set(60, 0, 220);
+
+
+
+// Tworzenie płaszczyzny dla tła i dodanie do sceny
+var backgroundGeometry = new THREE.PlaneGeometry(window.innerWidth, window.innerHeight); // Prosta geometria pokrywająca cały widok kamery
+var backgroundMesh = new THREE.Mesh(backgroundGeometry, backgroundShaderMaterial);
+scene.add(backgroundMesh);
+backgroundMesh.position.set(0, 0, -0.01); // Aby upewnić się, że jest za wszystkimi innymi obiektami
+
+
 
 // Function to resize the renderer and update camera aspect ratio accordingly
 function resizeRenderer() {
@@ -119,10 +179,10 @@ resizeRenderer();
 var width = canvas.offsetWidth;
 var height = canvas.offsetHeight;
 
-
-
 // Listen for window resize event to adjust renderer size and camera aspect ratio
 window.addEventListener('resize', resizeRenderer);
+
+
 
 // Create and add HemisphereLight to the scene
 var light = new THREE.HemisphereLight(0xffffff, 0x0C056D, 0.05);
@@ -255,3 +315,4 @@ window.addEventListener("resize", function () {
     resizeTm = clearTimeout(resizeTm);
     resizeTm = setTimeout(onResize, 200);
 });
+
